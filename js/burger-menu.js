1dbdfc07f8916bb174d3eed80a506d3e77cc7d4a
Miguel Lucendo <miguel.lucendo.esteban@gmail.com>
2022-11-18 18:35:39 +0100

    Menu hamburguesa añadido al index

---
+++ js/burger-menu.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    var burgerMenu = document.getElementById('burger-menu');
+    var overlay = document.getElementById('menu');
+    burgerMenu.addEventListener('click', function () {
+        this.classList.toggle("close");
+        overlay.classList.toggle("overlay");
+    });
+}
+
